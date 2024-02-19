a3e2d06cdcbe67aa5315135a7950acab062e16b4
Yusuf Kızılarslan <122723162+Yusufkizilarslan0@users.noreply.github.com>
2024-02-19 12:02:20 +0300

    Update Library Management System.py

--- Library Management System.py
+++ Library Management System.py
@@ -13,16 +13,31 @@ class library:
         else:
             for book in books:
                 book_info = book.split(",")
-                print(f"Title: {book_info[0]}, Author: {book_info[1]}, Release Year: {book_info[2]}, Pages: {book_info[3]}")
+                print(f"Title: {book_info[0]}, Author: {book_info[1]}")
 
     def add_book(self):
         title = input("Enter book title: ").title()
         author = input("Enter book author: ").title()
         release_year = input("Enter release year: ")
-        pages = input("Enter number of pages: ")
+"""
+        if len(release_year) != 4:
+            return ("Plase enter a 4-digit number.")
+        else:
+            print("Please wait.")
+"""
+        try:
+            released_year = int(release_year)
+        except ValueError:
+
+            return ("Invalid input ! Please use integer")
+        pages = input("Enter number of pages: ").title()
         book_info = f"{title},{author},{release_year},{pages}\n"
         self.file.write(book_info)
         print("Book added successfully.")
+    """
+    aynı kitap ve değerleri girilirse cevap döndür.
+    
+    """
 
 
     def remove_book(self):
@@ -32,9 +47,33 @@ class library:
         updated_books = [book for book in books if title not in book]
         self.file.seek(0)
         self.file.truncate()
-        for book in updated_books:
-            self.file.write(book + '\n')
-            print("Book removed successfully.")
+        for i in books:
+            if title not in books:
+                print("Book not found. Please check the title and try again.")
+                break
+            else:
+                self.file.write(book + '\n')
+                print("Book removed successfully.")
+
+"""
+  def all_remove_book(self):
+        password = 1234
+  
+        type(password) == int(password)
+        self.file.seek(0)
+        books = self.file.read().splitlines()
+        updated_books = [book for book in books if title not in book]
+        self.file.seek(0)
+        self.file.truncate()
+        password1 = int(input(" If you want delete all books, please enter the Password: "))
+
+        if password1 == password:
+            print("All books removed.")
+        else:
+            print("Wrong password !")
+
+
+"""
 
 lib = library()
 
@@ -44,6 +83,7 @@ while True:
     print("2) Add Book")
     print("3) Remove Book")
     print("4) press q to exit")
+    print("5) All Remove Books")
     choice = input("Enter your choice (1/2/3/4): ")
 
     if choice == "1":
@@ -56,5 +96,9 @@ while True:
         break
     elif choice == "q":
         break
+
+    elif choice == "5":
+        lib.all_remove_book()
+
     else:
         print("Invalid choice. Please enter 1, 2, 3, 4, or q.")
